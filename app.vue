fb99334ad34afa42b24c9d8922eaf2044e44382e
Daniel Spilker <daniel.spilker@bdess.com>
2023-10-17 21:11:45 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container py-4 px-3 mx-auto">
+    <nav class="navbar navbar-expand-md bg-primary-subtle">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Titel</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <NuxtLink to="/" class="nav-link" activeClass="active" ariaCurrentValue="page">Ferienwohnung</NuxtLink>
+            <NuxtLink to="/anfahrt" class="nav-link" activeClass="active">Anfahrt</NuxtLink>
+            <NuxtLink to="/kontakt" class="nav-link" activeClass="active">Kontakt</NuxtLink>
+            <NuxtLink to="/impressum" class="nav-link" activeClass="active">Impressum</NuxtLink>
+            <NuxtLink to="/datenschutz" class="nav-link" activeClass="active">Datenschutz</NuxtLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <NuxtPage />
+    
+    <div class="container">
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center">
+          <span class="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
+        </div>
+    
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li class="ms-3"><a class="text-muted" href="https://www.instagram.com/farmingsimulator/"><Icon name="uil:instagram"/></a></li>
+          <li class="ms-3"><a class="text-muted" href="https://www.facebook.com/MakitaDeutschland/"><Icon name="uil:facebook"/></a></li>
+        </ul>
+      </footer>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Ferienwohnung</h1>
+
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-lg-6">
+                <img src="~/assets/img/haus1.jpg" class="rounded img-fluid">
+            </div>
+            <div class="col-lg-6">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's
+                standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+                make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+                typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                PageMaker including versions of Lorem Ipsum.
+            </div>
+        </div>
+    </div>
+</template>
